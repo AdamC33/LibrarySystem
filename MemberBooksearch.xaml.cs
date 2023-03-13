e95c35deefac95d9891466a63be5564c710fdcf7
AdamC33 <ac1904@student.aru.ac.uk>
2023-03-13 16:55:32 +0000

    Added book search page without the known potential memory leak bug

--- MemberBooksearch.xaml.cs
+++ MemberBooksearch.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,20 @@ namespace LibrarySystem
     /// </summary>
     public partial class MemberBooksearch : Page
     {
-        public MemberBooksearch()
+        private Member _currentUser;
+
+        public MemberBooksearch(string searchQuery, byte searchBy, Member currentUser)
         {
+            int[] somearray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
             InitializeComponent();
+            _currentUser = currentUser;
+            listSearchResults.ItemsSource = somearray;
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.RemoveBackEntry();
+            NavigationService.Navigate(new MemberHomepage());
         }
     }
 }
--- MemberMainpage.xaml.cs
+++ MemberMainpage.xaml.cs
@@ -20,10 +20,44 @@ namespace LibrarySystem
     /// </summary>
     public partial class MemberMainpage : Page
     {
+        private Member _currentUser;
+
         public MemberMainpage(Member currentUser)
         {
             InitializeComponent();
+            _currentUser = currentUser;
             frameMember.Content = new MemberHomepage();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            byte searchBy;
+            if (radISBN.IsChecked == true)
+            {
+                searchBy = 0b00000001;
+            }
+            else if (radAuthor.IsChecked == true)
+            {
+                searchBy = 0b00000010;
+            }
+            else if (radYear.IsChecked == true)
+            {
+                searchBy = 0b00000011;
+            }
+            else if (radPublisher.IsChecked == true)
+            {
+                searchBy = 0b00000100;
+            }
+            else if (radCategory.IsChecked == true)
+            {
+                searchBy = 0b00000101;
+            }
+            else
+            {
+                searchBy = 0b00000000;
+            }
+            NavigationService.RemoveBackEntry();
+            frameMember.Content = new MemberBooksearch(txtSearch.Text, searchBy, _currentUser);
         }
     }
 }
